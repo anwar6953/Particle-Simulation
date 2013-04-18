6167d6ab442992918f906b9a60eeb5fd58904ee0
davemonster <daveucla08@gmail.com>
2013-04-17 18:39:01 -0700

    include Geometry.h working. One issue. macro on define sphAcc is used in both files. this should be resolved in as4.cpp since its only called from loadfile, instead of used all the time in the geometry.cpp

--- as4.cpp
+++ as4.cpp
@@ -1,5 +1,6 @@
 //{ Includes, Declarations of functions/classes:
 #include "ColorAndVector.h"
+#include "Geometry.h"
 
 #include <ctime>
 #include <vector>
@@ -25,16 +26,11 @@
 
 
 #define PI 3.14159265  // Should be used from mathlib
-#define sphAcc 20 //higher number => prettier spheres.
-#define thresholdForBounce 0 //higher number => bounces happen sooner.
+#define sphAcc 20
 
 inline float sqr(float x) { return x*x; }
 using namespace std;
 void myDisplay();
-
-class Viewport;
-class sphere;
-class plane;
 
 bool debug = false;
 int firstTime = 1;
@@ -60,121 +56,6 @@ void appendToFile(string fnameParam, string toAppend){
 
 //}
 
-//{ Classes (Viewport, pt):
-class Viewport {
-  public:
-    int w, h; // width and height
-};
-
-class plane {
-    public:
-     Vect3 pt;
-     Vect3 n;
-     float a,b,c,d;
-     plane();
-     plane(float, float, float, float);
-};
-plane::plane(){
-a=0;
-b=0;
-c=0;
-d=0;
-n=Vect3();
-pt=Vect3();
-}    
-plane::plane(float ap, float bp, float cp, float dp){
-a = ap;
-b = bp;
-c = cp;
-d = dp;
-n = Vect3();
-pt= Vect3();
-}
-
-class sphere {
-    public:
-     Vect3 pos;
-     Vect3 vel;
-     float r;
-     float m;
-     sphere();
-     sphere(Vect3, Vect3, float);
-     sphere(Vect3, Vect3, float, float);
-     void render();
-     bool intersect(sphere);
-     bool intersect(plane);
-     void move();
-     void drag();
-    };
-sphere::sphere(){
-pos = Vect3();
-vel = Vect3();
-r = 0;
-m = 1;
-}    
-sphere::sphere(Vect3 posp, Vect3 velp, float rp){
-pos = posp;
-vel = velp;
-r=rp;
-m = 1;
-}
-sphere::sphere(Vect3 posp, Vect3 velp, float rp, float massp){
-pos = posp;
-vel = velp;
-r=rp;
-m = massp;
-}
-void sphere::render(){
-    glTranslatef(pos.x,pos.y,pos.z);
-    
-    // glutWireSphere(r,sphAcc,sphAcc);
-    glutSolidSphere(r,sphAcc,sphAcc);
-    
-    glTranslatef(-pos.x,-pos.y,-pos.z);
-    
-    if(saveToFile){
-        if (counter != prevCounter){
-            appendToFile("test1","EOF\n");
-            prevCounter = counter;
-        }
-        string str = "";
-        ostringstream ss;
-        ss << pos.x << " " << pos.y << " " << pos.z << "\n";
-        appendToFile("test1",ss.str());
-    }
-}
-bool sphere::intersect(plane p){
-    float sumabcsquared = pow(p.a,2.0) + pow(p.b,2.0) + pow(p.c,2.0);
-    float xo = pos.x;
-    float yo = pos.y;
-    float zo = pos.z;
-    float firstTerm = p.a*xo + p.b*yo + p.c*zo + p.d;
-    float xc = xo - p.a * (firstTerm) / (sumabcsquared);
-    float yc = yo - p.b * (firstTerm) / (sumabcsquared);
-    float zc = zo - p.c * (firstTerm) / (sumabcsquared);
-
-    float d = abs(p.a*xo + p.b*yo + p.c*zo + p.d) / pow(sumabcsquared,0.5);
-    
-    if (r < d){ return false; }
-    else { return true; }
-}
-bool sphere::intersect(sphere s2){
-    float sumOfRadii = r + s2.r;
-    float distBetweenRadii = (pos - s2.pos).getNorm();
-    float surfaceDistance = distBetweenRadii - sumOfRadii;
-    if (surfaceDistance < thresholdForBounce) { return true; }
-    else { return false; }
-}
-void sphere::move(){   
-pos = pos + 1*vel;
-}
-void sphere::drag(){
-float dragCoef = 200;
-float force = 0.5 * vel * vel * dragCoef;
-float acc = (force / m);
-vel = vel - Vect3(acc, acc, acc);
-// vel = vel * (1 / vel.getNorm());
-}
 //}
 
 //{ Global Variables
